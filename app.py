e487c54fa129e741ec236fb376ab3a441b19e2cc
Angie-12 <70004028+Angie-12@users.noreply.github.com>
2020-10-20 14:29:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,61 @@
+print("Title of program: Post-exam bot")
+print()
+while True:
+  description = input("How do you feel after during the post exam activity?")
+
+  list_of_words = description.split()
+
+  feelings_list = []
+  encouragement_list = []
+  counter = 0
+  
+  for each_word in list_of_words:
+    
+    if each_word == "bored":
+      feelings_list.append("bored")
+      encouragement_list.append("google a joke lah")
+      counter += 1
+    if each_word == "results":
+      feelings_list.append("results")
+      encouragement_list.append("they do not define you")
+      counter += 1
+    if each_word == "tired":
+      feelings_list.append("tired")
+      encouragement_list.append("you are stronger than you think")
+    if each_word == "enjoying":
+      feelings_list.append("enjoying")
+      encouragement_list.append("share the fun")
+      counter += 1
+    if each_word == "lazy":
+      feelings_list.append("lazy")
+      encouragement_list.append("You can relax for awhile but remember to plan your time wisely.")
+      counter += 1
+
+  if counter == 0:
+    
+      output = "Sorry I don't really understand. Please use different words?"
+
+  elif counter == 1:
+    
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+
+  else:
+
+    feelings = ""    
+    for i in range(len(feelings_list)-1):
+      feelings += feelings_list[i] + ", "
+    feelings += "and " + feelings_list[-1]
+    
+    encouragement = ""    
+    for j in range(len(encouragement_list)-1):
+      encouragement += encouragement_list[i] + ", "
+    encouragement += "and " + encouragement_list[-1]
+
+    output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
+
+  print()
+  print(output)
+  print()
+
+  
 
